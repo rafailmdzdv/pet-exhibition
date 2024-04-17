c9e0db4b1ce5d56f1284758142980da27ed2deba
rafailmdzdv <rafayt323@xmail.ru>
2024-04-17 22:26:32 +0300

    add cat domain entity

---
+++ src/domain/enums/color.py
@@ -0,0 +1,5 @@
+from enum import StrEnum
+
+
+class Colors(StrEnum):
+    BLACK = "black"
---
+++ tests/unit/domain/cat/test_create_cat.py
@@ -0,0 +1,15 @@
+from src.domain.cat.entity import Cat
+from src.domain.enums.breed import Breeds
+from src.domain.enums.color import Colors
+
+
+def test_create_cat() -> None:
+    cat = Cat.create(
+        name="Bob",
+        breed=Breeds.cat.SCOTTISH_STRAIGHT,
+        age=3,
+        color=Colors.BLACK,
+    )
+    assert cat.name == "Bob"
+    assert cat.breed == "Scottish Straight"
+    assert cat.color == "black"
